77cf1b48bbbe0111b1f0d1bc71413a21b2ed4837
rek7 <rek7@protonmail.com>
2019-12-06 02:22:23 -0500

    timer added

---
+++ server/timer.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+var poll_seconds = 10
+
+var teamWorkers = [
+    "1982.1.1.0:8032",
+    "1982.1.1.0:8039"
+]
+
+var services = [
+    "MYSQL_ALL",
+    "AD_ALL",
+    "DNS_ALL",
+    "ICMP_ALL",
+    "WEB_ALL",
+    "SSH_ALL"
+]
+
+function poll() {
+    for (index = 0; index < teamWorkers.length; index++) { 
+        for (service_index = 0; service_index < services.length; service_index++) { 
+            var api = "http://" + teamWorkers[index] + "/" + services[service_index] + "/"
+            var endpoint = api + (index+1)
+            console.log(endpoint)
+            axios.get(endpoint)
+            .then(function (response) {
+            })
+            .catch(function (error) {
+           //   console.log(error);
+            })
+        }
+    }
+}
+
+setInterval(poll, poll_seconds*1000)
